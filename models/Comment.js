8a076f7576cb5933de53c1239a0f37807d40638d
Joseph Prospero <jprospero413@gmail.com>
2021-04-16 18:23:07 -0700

    add validation

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,13 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      trim: true,
+      required: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
@@ -27,21 +30,24 @@ const ReplySchema = new Schema(
   }
 );
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+      required: true
+    },
+    commentBody: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  },
-  // use ReplySchema to validate data for a reply
-  replies: [ReplySchema]
-},
   {
     toJSON: {
       virtuals: true,
